24a21686fd6b9a601c262738b8723e6afce3033f
kou-saka <juntakasaka5240@gmail.com>
2025-07-01 16:01:23 +0900

    課題完了

--- answer/generate_test_data.sh
+++ answer/generate_test_data.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 # このファイルに回答を記述してください
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <start_date> <end_date>"
+  exit 1
+fi
+
+START_DATE=$1
+END_DATE=$2
+
+# コンテナ名
+CONTAINER_NAME=sql-batch-practice-db
+
+# psqlでPostgreSQLコンテナに接続
+docker exec -i "$CONTAINER_NAME" psql -U postgres -d practice_db <<EOF
+SELECT generate_test_data('$START_DATE', '$END_DATE');
+EOF
